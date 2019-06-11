5b738b8590101193cbc9f0489f79f0c4201636ea
hghgobi <569258059@qq.com>
2019-06-11 17:40:35 +0800

    sgs

--- mylearn/views.py
+++ mylearn/views.py
@@ -1079,21 +1079,23 @@ def homeworkg(request):
 @csrf_exempt
 def weixin_main(request):
     if request.method=='GET':
-        signature = request.GET.get('signature',None)
-        timestamp = request.GET.get('timestamp',None)
-        nonce = request.GET.get('nonce',None)
-        echostr= request.GET.get('echostr',None)
+        signature = str(request.GET.get('signature',None))
+        timestamp =str(request.GET.get('timestamp',None))
+        nonce = str(request.GET.get('nonce',None))
+        echostr= str(request.GET.get('echostr',None))
 
-        token = 'hghgobi'
+        token = 'hghgobi7727'
 
         hashlist = [token,timestamp,nonce]
         hashlist.sort()
-        hashstr = ''.join([s for s in hashlist])
-        hashstr=hashlib.sha1(hashstr).hexdigest()
+        hashstr = ''
+        for i in hashlist:
+            hashstr+=i
+        hashstr = hashlib.sha1(hashstr.encode(encoding="UTF-8")).hexdigest()
         if hashstr == signature:
             return HttpResponse(echostr)
         else:
-            return HttpResponse("field")
+            return HttpResponse("error")
     else:
         othercontent = "999"
         return HttpResponse(othercontent)
