871f4b7f37af6db29fca8fbe53d9128ff20442e9
hghgobi <569258059@qq.com>
2019-10-19 11:08:30 +0800

    ss

--- mylearn/views.py
+++ mylearn/views.py
@@ -763,16 +763,43 @@ def Showwkqs2(request,id0,id1):
                 qsanswer2.append(qss.questionanswer2)
                 qsid.append(qss.pk)
                 categorys.append(qss.category)
-            if categorys[0]==3:
-                return render(request,'showqs3.html',{'testrm':json.dumps(testrm),'qstext':json.dumps(qstext),'qsanswer1':json.dumps(qsanswer1),'qsanswer2':json.dumps(qsanswer2),'qsid':qsid,'qsamount':json.dumps(qsamount),'zid':zid,'jid':jid,'limit':json.dumps(limit)})
-            else:
-                return render(request, 'showqs4.html',
-                              {'testrm':json.dumps(testrm),'qstext': json.dumps(qstext), 'qsanswer1': json.dumps(qsanswer1),
-                               'qsanswer2': json.dumps(qsanswer2), 'qsid': qsid, 'qsamount': json.dumps(qsamount),
-                               'zid': zid, 'jid': jid, 'limit': json.dumps(limit)})
+            # if categorys[0]==3:
+            return render(request,'showqs3.html',{'testrm':json.dumps(testrm),'qstext':json.dumps(qstext),'qsanswer1':json.dumps(qsanswer1),'qsanswer2':json.dumps(qsanswer2),'qsid':qsid,'qsamount':json.dumps(qsamount),'zid':zid,'jid':jid,'limit':json.dumps(limit)})
+            # else:
+            #     return render(request, 'showqs4.html',
+            #                   {'testrm':json.dumps(testrm),'qstext': json.dumps(qstext), 'qsanswer1': json.dumps(qsanswer1),
+            #                    'qsanswer2': json.dumps(qsanswer2), 'qsid': qsid, 'qsamount': json.dumps(qsamount),
+            #                    'zid': zid, 'jid': jid, 'limit': json.dumps(limit)})
         else:
             ms = '已通过本节测试，无需重复测试！可前往尚未测试的'
             return render(request, 'yuxi.html', {'ms': ms})
+
+    if request.method == 'POST':
+        teststudent = request.session.get("teststudent")
+        if not teststudent:
+            return redirect('../testlogin')
+
+        costtime = request.POST.get('time')
+        id0 = request.POST.get('zid')
+        id1 = request.POST.get('jid')
+        if Newnames.objects.filter(zid=id0, jid=id1, name=teststudent):
+            pass
+        else:
+            ms = '已通过本节测试，无需重复测试！可前往尚未测试的'
+            return render(request, 'yuxi.html', {'ms': ms})
+
+        counts =get_object_or_404(Yuxitestcount,zid = id0,jid = id1,name = teststudent)
+        nnnn = counts.count
+        ornot = "已通过本节测试"
+        Yuxiname.addyxname(id0, id1,teststudent,ornot,nnnn,costtime)
+        try:
+            Newnames.objects.filter(zid=id0,jid=id1,name=teststudent).delete()
+        except:
+            pass
+        ms = Yuxiname.objects.filter(zid=id0, jid=id1)
+        mss = Newnames.objects.filter(zid=id0, jid=id1)
+        n = len(mss)
+        return render(request, 'yuxiname2.html', {'ms': ms, 'id0': id0, 'id1': id1, 'mss': mss, 'n': n})
 
 
 def Testresult(request):
