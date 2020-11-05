53116c5e70b6b48f8634a147f46387954ac5eb08
hghgobi <569258059@qq.com>
2020-11-05 17:56:58 +0800

    ssg

--- mylearn/views.py
+++ mylearn/views.py
@@ -3697,51 +3697,51 @@ def addqs2(request,yunsuan):
         while d > 0:
             a = random.randint(-100, 100)
             b = random.randint(-100, 100)
-            if (a > 0 and b > 0) or b == 0 or a==0:
-                pass
+            if a > 0 and b > 0:
+                continue
+            elif b == 0 or a == 0:
+                continue
             else:
+                e = a
+                f = b
                 if a % b == 0:
                     num0 = a
                     num1 = b
-                    c=int(a/b)
+                    c = int(a / b)
                     answer = str(c)
                     ornot = 0
-                    Xxqs23.addqs3(num0, num1, yunsuan,answer, ornot)
+                    Xxqs23.addqs3(num0, num1, yunsuan, answer, ornot)
                     d = d - 1
 
                 else:
                     if abs(a) > abs(b):
-                        e=0
-                        f=0
                         for i in range(2, abs(a)):
                             while a % i == 0 and b % i == 0:
-                                e = a // i
-                                f = b // i
-                        if a*b>0:
-                            c = str(abs(e)) + 'V' + str(abs(f))
+                                a = a // i
+                                b = b // i
+                        if a * b > 0:
+                            c = str(abs(a)) + 'V' + str(abs(b))
                         else:
-                            c = '-'+str(abs(e)) + 'V' + str(abs(f))
-                        num0 = a
-                        num1 = b
+                            c = '-' + str(abs(a)) + 'V' + str(abs(b))
+                        num0 = e
+                        num1 = f
                         answer = str(c)
                         ornot = 0
                         Xxqs23.addqs3(num0, num1, yunsuan, answer, ornot)
                         d = d - 1
                     else:
-                        e=0
-                        f=0
                         for i in range(2, abs(b)):
                             while a % i == 0 and b % i == 0:
-                                e = a // i
-                                f = b // i
-                        if a*b>0:
-                            c = str(abs(e)) + 'V' + str(abs(f))
+                                a = a // i
+                                b = b // i
+                        if a * b > 0:
+                            c = str(abs(a)) + 'V' + str(abs(b))
                         else:
-                            c = '-'+str(abs(e)) + 'V' + str(abs(f))
+                            c = '-' + str(abs(a)) + 'V' + str(abs(b))
                         answer = str(c)
                         ornot = 0
-                        num0 = a
-                        num1 = b
+                        num0 = e
+                        num1 = f
                         Xxqs23.addqs3(num0, num1, yunsuan, answer, ornot)
                         d = d - 1
 
