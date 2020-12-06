31d2bbb175b17a660f860ad4a136a6e8a0694477
hghgobi <569258059@qq.com>
2020-12-06 10:31:51 +0800

    ss

--- mylearn/views.py
+++ mylearn/views.py
@@ -24,7 +24,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.dates import DateFormatter
 import matplotlib.pyplot as plt
 from pylab import *
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+# from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 import datetime
 import base64
 from io import BytesIO
@@ -4977,21 +4977,15 @@ def Drawpic(request):
         # plt.ylabel("频率")
         # plt.xlabel("抛掷总次数")
         # plt.tick_params(axis='both', which='major', labelsize=8)
-        # xmajorLocator = MultipleLocator(20)  # 将x主刻度标签设置为20的倍数
-        # xmajorFormatter = FormatStrFormatter('%1.0f')  # 设置x轴标签文本的格式
-        # xminorLocator = MultipleLocator(5)  # 将x轴次刻度标签设置为5的倍数
-        ymajorLocator = MultipleLocator(0.1)  # 将y轴主刻度标签设置为0.5的倍数
-        ymajorFormatter = FormatStrFormatter('%.5f')  # 设置y轴标签文本的格式
-        yminorLocator = MultipleLocator(0.01)  # 将此y轴次刻度标签设置为0.1的倍数
-        ax = subplot(111)  # 注意:一般都在ax中设置,不再plot中设置
+
+        ymajorLocator = MultipleLocator(0.1)
+        ymajorFormatter = FormatStrFormatter('%.5f')
+        yminorLocator = MultipleLocator(0.01)
+        ax = subplot(111)
         plot(dates, scores,c='red')
-        # 设置主刻度标签的位置,标签文本的格式
-        # ax.xaxis.set_major_locator(xmajorLocator)
-        # ax.xaxis.set_major_formatter(xmajorFormatter)
         ax.yaxis.set_major_locator(ymajorLocator)
         ax.yaxis.set_major_formatter(ymajorFormatter)
-        # 显示次刻度标签的位置,没有标签文本
-        # ax.xaxis.set_minor_locator(xminorLocator)
+
         ax.yaxis.set_minor_locator(yminorLocator)
         ax.xaxis.grid(True, which='major')  # x坐标轴的网格使用主刻度
         ax.yaxis.grid(True, which='minor')  # y坐标轴的网格使用次刻度
