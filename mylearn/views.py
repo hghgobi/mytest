f902447a5a725fde1d1f931446b9a3eb20dd5429
hghgobi <569258059@qq.com>
2020-12-07 18:53:05 +0800

    s

--- mylearn/views.py
+++ mylearn/views.py
@@ -5008,8 +5008,10 @@ def Drawpic(request):
         scoress=[]
         for i in range(len(scores)):
             scoress.append(0.5)
-        plot(dates, scoress, c='blue')
+
         plot(dates, scores,'--b*',c='red')
+        dates.insert(0,'0')
+        plot(dates, scoress, c='blue')
         ax.yaxis.set_major_locator(ymajorLocator)
         ax.yaxis.set_major_formatter(ymajorFormatter)
 
